7cbddd5d8eef4bd380c255f089ea08b10b7a110a
Reaper <ahoy@barelyhuman.dev>
2022-08-28 16:58:43 +0530

    Initial Commit

---
+++ mark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mark
+//
+//  Created by Reaper Gelera on 28/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mark/markApp.swift
@@ -0,0 +1,17 @@
+//
+//  markApp.swift
+//  mark
+//
+//  Created by Reaper Gelera on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct markApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
